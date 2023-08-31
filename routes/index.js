122fd250efaa70f8af4d58af3ddd8147eb7fd0a4
MuhammadWildanf <wildanf.daftar@gmail.com>
2023-08-31 13:49:57 +0700

    ok

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,21 @@ const express = require('express')
 const Controller = require('../controllers/controller')
 const UserController = require('../controllers/Usercontroller')
 const router = express.Router()
-const login = require('./login')
 
+router.get('/register', UserController.registerForm)
+router.post('/register', UserController.register)
 
+//login
+router.get('/login', UserController.loginForm)
+router.post('/login', UserController.login)
+
+router.use(function (req,res,next){
+    console.log('test ya');
+    next()
+})
 
 router.get('/', Controller.home)
 
-router.use(login)
 
 
 module.exports = router
