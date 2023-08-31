e0e7499850d48bee42ced04c92912f32c00b3ea3
MuhammadWildanf <wildanf.daftar@gmail.com>
2023-09-01 01:26:24 +0700

    new

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const midleware = require('../middlewares/auth')
 const routerEvent = require('./routerEvent')
 const routerUser = require('./routerUser')
-const UserController = require('../controllers/UserController')
+const UserController = require('../controllers/Usercontroller')
 
 router.get('/', Controller.home)
 router.get('/register', UserController.registerForm)
